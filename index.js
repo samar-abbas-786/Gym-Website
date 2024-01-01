802f8e6aac10df5ea7c3e02e8db857e531b0431c
Samar Abbas <samarabbas172003@gmail.com>
2024-01-01 22:28:09 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+//location
+function Go_on_Map() {
+  var url = "https://maps.app.goo.gl/JLKLnWmCMiC2eqsX6";
+
+  window.open(url, "_blank");
+}
+function openInsta() {
+  var url = "https://www.instagram.com/cross_way_fitness_club/#";
+  window.open(url, "_blank");
+}
+function openFb() {
+  var url =
+    "https://www.facebook.com/profile.php?id=100064169886526&mibextid=gik2fB";
+  window.open(url, "_blank");
+}
+
+function Membership() {
+  var url = "https://forms.gle/koMtiTGf769orR1i9";
+  window.open(url, "_blank");
+}
